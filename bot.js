df1ed8381047e0f5fb695d9824603f5450a0863b
Fofodiscord <42048326+discordyou23@users.noreply.github.com>
2018-09-03 02:54:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -427,7 +427,29 @@ client.on('message', message => {
 });
 
 
-  
+ 
+
+
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`👑join Member👑`)
+    .setDescription(`👋Welcome To Server👋`)
+    .addField('You Number-أنت رقم',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RANDOM')
+    .setFooter('DyzerYT | دايزر BOT')
+
+var channel =member.guild.channels.find('name', 'welcome')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+
+
+
+
+
   client.on("message", msg => {
            var prefix = "D";
   if(msg.content.startsWith (prefix + "id")) {
@@ -454,37 +476,17 @@ client.on('message', message => {
 
 
 
-    client.on('guildMemberAdd', member => {
-        let channel = member.guild.channels.find('name', 'welcome');
-        let memberavatar = member.user.avatarURL
-          if (!channel) return;
-        let embed = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setThumbnail(memberavatar)
-            .addField('🎽 | name :  ',`${member}`)
-            .addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
-            .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
-                    .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-                   
-                      .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                         
-                                         .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                           
-         .setFooter(`${member.guild.name}`)
-            .setTimestamp()
-       
-          channel.sendEmbed(embed);
-        });
+
         
         client.on('guildMemberRemove', member => {
             var embed = new Discord.RichEmbed()
             .setAuthor(member.user.username, member.user.avatarURL)
             .setThumbnail(member.user.avatarURL)
-            .setTitle(`الله معاك ✋:skin-tone-1: 😔`)
-            .setDescription(`مع السلامه تشرفنا بك ✋:skin-tone-1: 😔 `)
-            .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-            .setColor('RED')
-            .setFooter(`==== نــتــمــنــآ لــكــم آســتــمـــتــآع ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+            .setTitle(`😣GOOD BAY-الله معاك💔`)
+            .setDescription(`😠بس أذا كنت مساوى شي بلا رجعا👊`)
+            .addField('ℹتبقى بعد خروج العضو✅',`**[ ${member.guild.memberCount} ]**`,true)
+            .setColor('RANDOM')
+            .setFooter('DyzerYT | دايزر')
         
         var channel =member.guild.channels.find('name', 'leave')
         if (!channel) return;
